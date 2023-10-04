c607aa550001df4f89acdfc108273a26501316ef
Jonathan Mumm <jon@explorers.club>
2023-10-04 16:19:21 -0700

    fix value

--- src/app/_trpc/index.ts
+++ src/app/_trpc/index.ts
@@ -2,3 +2,5 @@ export const trpcUrl =
   process.env.NODE_ENV === "production"
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
     : "http://localhost:3000/api/trpc";
+
+console.log({ trpcUrl, ENV_VL: process.env.NEXT_PUBLIC_VERCEL_URL });
