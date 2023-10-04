2f99268757e1b130d85a9d8791ba87f229af9e89
Jonathan Mumm <jon@explorers.club>
2023-10-04 16:22:48 -0700

    go back to next url

--- src/app/_trpc/index.ts
+++ src/app/_trpc/index.ts
@@ -1,6 +1,6 @@
 export const trpcUrl =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL}/api/trpc`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
     : "http://localhost:3000/api/trpc";
 
-console.log({ trpcUrl, ENV_VL: process.env.VERCEL_URL });
+console.log({ trpcUrl, ENV_VL: process.env.NEXT_PUBLIC_VERCEL_URL });
