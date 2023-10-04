e8a96991ed55d3773e6ba822c42b3d3bbbb28898
Jonathan Mumm <jon@explorers.club>
2023-10-04 16:18:59 -0700

    logging for deployment debug

--- src/app/_trpc/index.ts
+++ src/app/_trpc/index.ts
@@ -1,4 +1,4 @@
 export const trpcUrl =
   process.env.NODE_ENV === "production"
-    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
     : "http://localhost:3000/api/trpc";
