2ff6effc7747cf0816d13601d1599a876f436349
Jonathan Mumm <jon@explorers.club>
2023-10-07 15:45:58 -0700

    add title and description to recipe pages

--- src/app/recipe/[slug]/page.tsx
+++ src/app/recipe/[slug]/page.tsx
@@ -3,8 +3,10 @@ import RecipeViewer from "@/components/recipe-viewer";
 import { getLLMMessageSet, getRecipe } from "@/lib/db";
 import { kv } from "@vercel/kv";
 import { z } from "zod";
-import Header from "./header";
+import Head from "next/head";
 import Provider from "./provider";
+import { Metadata, ResolvingMetadata } from "next";
+import { Props } from "next/script";
 
 const getSessionId = (cookies: string) => {
   return "";
@@ -24,20 +26,43 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const messages = recipe.messageSet
     ? await getLLMMessageSet(kv, recipe.messageSet)
     : [];
+  const title = `${recipe.name} | KitchenCraft.ai`;
 
   return (
-    <Provider
-      chatId={chatId}
-      userId={userId}
-      recipe={recipe}
-      sessionId={sessionId}
-      recipeMessages={messages}
-    >
-      <div className="flex flex-col flex-end flex-1 justify-end overflow-hidden">
-        {/* <Header /> */}
-        <RecipeViewer />
-        <RecipeChat />
-      </div>
-    </Provider>
+    <>
+      <Provider
+        chatId={chatId}
+        userId={userId}
+        recipe={recipe}
+        sessionId={sessionId}
+        recipeMessages={messages}
+      >
+        <div className="flex flex-col flex-end flex-1 justify-end overflow-hidden">
+          {/* <Header /> */}
+          <RecipeViewer />
+          <RecipeChat />
+        </div>
+      </Provider>
+    </>
   );
+}
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const recipe = await getRecipe(kv, params.slug);
+  const title = `${recipe.name} | KitchenCraft.ai`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      description: recipe.description,
+    },
+  };
 }
