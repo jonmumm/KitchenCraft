8e65970898142f194452f855c6b779ed07c07b8a
Jonathan Mumm <jon@explorers.club>
2023-10-08 15:16:18 -0700

    add header to chef page

--- src/app/chef/[username]/page.tsx
+++ src/app/chef/[username]/page.tsx
@@ -1,7 +1,16 @@
+import { Header } from "@/app/header";
+
 type Props = {
   params: { username: string };
 };
 
 export default async function Page({ params }: Props) {
-  return <h2 className="text-lg font-semibold">{params.username}</h2>;
+  return (
+    <div className="max-w-2xl mx-auto">
+      <Header />
+
+      <h1>🧪 {params.username}</h1>
+      <h2>Chef</h2>
+    </div>
+  );
 }
