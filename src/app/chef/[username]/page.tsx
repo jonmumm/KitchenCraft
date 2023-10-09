d6826f91dd7a39fbbfdc093471e766c1cc22a1e4
Jonathan Mumm <jon@explorers.club>
2023-10-09 06:07:29 -0700

    add recent crafts to chef page

--- src/app/chef/[username]/page.tsx
+++ src/app/chef/[username]/page.tsx
@@ -1,4 +1,9 @@
 import { Header } from "@/app/header";
+import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { ChefHatIcon, ScrollTextIcon } from "lucide-react";
+import { RecentCard } from "./recent-card";
 
 type Props = {
   params: { username: string };
@@ -6,11 +11,28 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto flex flex-col items-center">
       <Header />
 
-      <h1>🧪 {params.username}</h1>
-      <h2>Chef</h2>
+      <div className="px-4 w-full flex flex-col gap-4">
+        <Card className="py-3 w-full">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-row gap-1 items-center px-2">
+              <div className="px-4">
+                <ChefHatIcon />
+              </div>
+              <div className="flex flex-col gap-1">
+                <h1 className="underline font-bold text-xl">
+                  {params.username}
+                </h1>
+                <span className="font-medium text-sm">(+123 🧪)</span>
+              </div>
+            </div>
+            <Separator />
+          </div>
+        </Card>
+        <RecentCard />
+      </div>
     </div>
   );
 }
