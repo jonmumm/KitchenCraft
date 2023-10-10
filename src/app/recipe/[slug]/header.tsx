6c46695c683a114c3f5aaa79e1b6e2399d67ec93
Jonathan Mumm <jon@explorers.club>
2023-10-10 12:47:55 -0700

    fix header

--- src/app/recipe/[slug]/header.tsx
+++ src/app/recipe/[slug]/header.tsx
@@ -1,18 +1,5 @@
 "use client";
 
-import { RecipeChatContext } from "@/components/recipe-chat";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
-import { useSelector } from "@/hooks/useSelector";
-import { ArrowBigUpDashIcon, PrinterIcon, SaveIcon } from "lucide-react";
-import { useCallback, useContext, useState } from "react";
-
 export default function Header() {
-  const actor = useContext(RecipeChatContext);
-  // todo?
-
   return null;
 }
