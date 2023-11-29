c18a69558f2b2db28e3c013203e4346d488f77b3
Jonathan Mumm <jon@explorers.club>
2023-11-29 07:14:14 -0800

    remove log

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,6 @@ import type { Config } from "drizzle-kit";
 
 import "dotenv/config";
 
-console.log("HIII", process.env.POSTGRES_URL)
-
 export default {
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
