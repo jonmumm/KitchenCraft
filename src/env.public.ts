2141cc7ad62093707689e7d6c841c76fa44069c5
Jonathan Mumm <jon@explorers.club>
2023-12-04 14:47:34 -0800

    Try vercel url

--- src/env.public.ts
+++ src/env.public.ts
@@ -3,6 +3,6 @@ import { PublicEnvironmentSchema } from "./schema";
 export const env = PublicEnvironmentSchema.parse({
   KITCHENCRAFT_URL:
     process.env.NEXT_PUBLIC_KITCHENCRAFT_URL ||
-    `https://${process.env.VERCEL_URL}`,
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
   ADSENSE_PUBLISHER_ID: process.env.NEXT_PUBLIC_ADSENSE_PUBLISHER_ID,
 });
