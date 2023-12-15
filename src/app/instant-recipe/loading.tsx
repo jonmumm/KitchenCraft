ba5f8f81f4c500d0e8adb287b9942dc374a84bce
Jonathan Mumm <jon@explorers.club>
2023-12-15 09:40:14 -0800

    remove header loading

--- src/app/instant-recipe/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderLoading } from "../header";
-
-export default function Loading() {
-  return (
-    <div className="max-w-2xl mx-auto w-full">
-      <HeaderLoading />
-    </div>
-  );
-}
--- src/app/recipe/[slug]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderLoading } from "@/app/header";
-
-export default async function Loading() {
-  return (
-    <div className="max-w-7xl mx-auto w-full">
-      <HeaderLoading showBack={true} />
-    </div>
-  );
-}
