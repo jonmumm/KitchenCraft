3b8fd078f924b1a0b242772a3d89f21b1a95d9c0
Jonathan Mumm <jonathanrmumm@gmail.com>
2023-12-28 14:58:51 -0800

    fix empty state for no tag

--- src/app/tag/[tagSlug]/page.tsx
+++ src/app/tag/[tagSlug]/page.tsx
@@ -1,8 +1,12 @@
 import { RecipeListItem } from "@/app/recipe/components";
+import { Badge } from "@/components/display/badge";
+import { Card } from "@/components/display/card";
 import { Separator } from "@/components/display/separator";
+import { Button } from "@/components/input/button";
 import { TagsCarousel } from "@/components/modules/tags-carousel";
 import { getRecipesByTag } from "@/db/queries";
 import { slugToSentence } from "@/lib/utils";
+import { ChevronRightIcon } from "lucide-react";
 
 export default async function Page(props: { params: { tagSlug: string } }) {
   const tag = slugToSentence(props.params.tagSlug);
@@ -21,9 +25,15 @@ export default async function Page(props: { params: { tagSlug: string } }) {
             ))}
           </div>
         ) : (
-          <div>
-            No recipes found for <span className="italics">${tag}</span>
-          </div>
+          <Card className="mx-4 p-4 flex flex-col justify-center items-center gap-4">
+            <h1 className="font-semibold txt-lg">Oh no</h1>
+            <div className="flex flex-row items-center gap-2 justify-center text-sm">
+              No recipes found matching <Badge variant="outline">{tag}</Badge>
+            </div>
+            <Button event={{ type: "NEW_RECIPE" }} variant="secondary" className="flex flex-row gap-1 items-center">
+              Create One <ChevronRightIcon />
+            </Button>
+          </Card>
         )}
       </div>
     </div>
