4defe7d37d00658a6763f7a3844c647aeb3281bc
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-01-06 21:44:50 -0800

    fix render causing keyboard lose focus

--- src/hooks/useSelectorCallback.ts
+++ src/hooks/useSelectorCallback.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from "react";
-import { useSelector } from "./useSelector"; // assuming this is the path to your useSelector function
 import type { ActorRef, SnapshotFrom } from "xstate";
 
 export function useSelectorCallback<TActor extends ActorRef<any, any>, T>(
@@ -8,16 +7,18 @@ export function useSelectorCallback<TActor extends ActorRef<any, any>, T>(
   callback: (currentValue: T, prevValue: T | undefined) => void,
   compare: (prev: T | undefined, next: T) => boolean = defaultCompare
 ): void {
-  const currentValue = useSelector(actor, selector);
   const prevValueRef = useRef<T | undefined>(undefined);
 
   useEffect(() => {
-    const prevValue = prevValueRef.current;
-    if (prevValue === undefined || !compare(prevValue, currentValue)) {
-      callback(currentValue, prevValue);
-    }
-    prevValueRef.current = currentValue;
-  }, [currentValue, callback, compare]);
+    return actor.subscribe((state) => {
+      const currentValue = selector(state);
+      const prevValue = prevValueRef.current;
+      if (prevValue === undefined || !compare(prevValue, currentValue)) {
+        callback(currentValue, prevValue);
+      }
+      prevValueRef.current = currentValue;
+    }).unsubscribe;
+  }, [actor, selector, callback, compare]);
 }
 
 function defaultCompare<T>(a: T | undefined, b: T): boolean {
