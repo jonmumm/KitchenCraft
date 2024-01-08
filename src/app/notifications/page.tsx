f75a24928c38bc5c05d24e1c5cf7739f19a94f41
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-01-08 15:30:07 -0800

    redirect if not logged in

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -12,39 +12,16 @@ import {
 } from "@/components/display/card";
 import { Label } from "@/components/display/label";
 import { Switch } from "@/components/input/switch";
+import { getCurrentUserId } from "@/lib/auth/session";
+import { redirect } from "next/navigation";
 
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/yrYLqMRUzD4
- */
-
-export default function Component() {
-  const NotificationItem = ({
-    title,
-    description,
-  }: {
-    title: string;
-    description: string;
-  }) => {
-    return (
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-col gap-1">
-          <div className="font-medium">{title}</div>
-          <div className="text-sm text-gray-500">{description}</div>
-        </div>
-        <div className="flex justify-end space-x-4">
-          <div className="flex flex-col gap-1 items-center">
-            <Switch id="push-new-releases" />
-            <Label className="text-xs text-muted-foreground">Push</Label>
-          </div>
-          <div className="flex flex-col gap-1 items-center">
-            <Switch id="email-new-releases" />
-            <Label className="text-xs text-muted-foreground">Email</Label>
-          </div>
-        </div>
-      </div>
+export default async function Page() {
+  const currentUserId = await getCurrentUserId();
+  if (!currentUserId) {
+    redirect(
+      `/auth/signin?callbackUrl=${encodeURIComponent(`/notifications`)}`
     );
-  };
+  }
 
   return (
     <main className="flex flex-col items-center justify-center h-full p-4 md:p-8">
@@ -75,3 +52,30 @@ export default function Component() {
     </main>
   );
 }
+
+const NotificationItem = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-col gap-1">
+        <div className="font-medium">{title}</div>
+        <div className="text-sm text-gray-500">{description}</div>
+      </div>
+      <div className="flex justify-end space-x-4">
+        <div className="flex flex-col gap-1 items-center">
+          <Switch id="push-new-releases" />
+          <Label className="text-xs text-muted-foreground">Push</Label>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <Switch id="email-new-releases" />
+          <Label className="text-xs text-muted-foreground">Email</Label>
+        </div>
+      </div>
+    </div>
+  );
+};
