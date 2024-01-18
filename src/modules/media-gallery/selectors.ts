7625b6e9add693b3f4a8f2dbb42c0132a2c02014
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-01-18 09:59:32 -0800

    fix

--- src/modules/media-gallery/selectors.ts
+++ src/modules/media-gallery/selectors.ts
@@ -2,7 +2,7 @@ import { createSelector } from "reselect";
 import { MediaGallerySnapshot } from "./machine";
 
 export const selectIsFullscreen = (state: MediaGallerySnapshot) => {
-  return state.matches("Fullscreen.True");
+  return state.matches({ Fullscreen: "True" });
 };
 
 export const selectContext = (state: MediaGallerySnapshot) => {
