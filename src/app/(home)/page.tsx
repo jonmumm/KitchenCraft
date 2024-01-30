6b42eba913889023aa2bd1563e3164c212dae1d6
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-01-30 09:57:14 -0800

    add quickstart link

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,10 @@
+import { Card } from "@/components/display/card";
+import { Button } from "@/components/input/button";
 import { getSession } from "@/lib/auth/session";
+import { ChevronsRightIcon, XIcon } from "lucide-react";
 import { getHotRecipes } from "../../db/queries";
 import { RecipeListItem } from "../recipe/components";
+import Link from "next/link";
 
 // export const dynamic = "force-dynamic";
 export default async function Page() {
@@ -11,6 +15,23 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col sm:gap-10 mt-0 sm:mt-10">
+      <Card className="mx-auto max-w-md w-full p-4 rounded-2xl flex flex-col gap-3">
+        <div className="flex flex-row justify-between w-full items-center">
+          <h3 className="text-xl font-semibold">🙋First Timer?</h3>
+          <Button variant="outline" size="icon">
+            <XIcon />
+          </Button>
+        </div>
+        <p className="opacity-70 text-med">
+          Get started with KitchenCraft to instantly create personalized
+          recipes.
+        </p>
+        <Link className="flex flex-row justify-end" href="/quick-start">
+          <Button>
+            Quick Start <ChevronsRightIcon className="ml-1" size={16} />
+          </Button>
+        </Link>
+      </Card>
       {items.map((_, index) => {
         const recipe = recipes[index];
 
