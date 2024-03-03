18d8260fc9878b6f18b27b5b93d4421edf6e9a6f
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-03-03 07:02:19 -0800

    fix card jumpiness

--- src/app/@craft/components.client.tsx
+++ src/app/@craft/components.client.tsx
@@ -383,9 +383,10 @@ export const SuggestedRecipeCard = ({ index }: { index: number }) => {
   }
 
   const scale = (100 - diffToCurrent * 5) / 100;
-  const topOffset = diffToCurrent
-    ? -4 * (1 + (1 - scale)) - diffToCurrent * 28
-    : 0;
+  const topOffset = -diffToCurrent * 7;
+  // const topOffset = diffToCurrent
+  //   ? -4 * (1 + (1 - scale)) - diffToCurrent * 28
+  //   : 0;
   const zIndex = 50 - diffToCurrent * 10;
   const position = diffToCurrent ? "absolute" : "relative";
   // const overflow = diffToCurrent !== 0 ? "hidden" : "";
@@ -403,7 +404,7 @@ export const SuggestedRecipeCard = ({ index }: { index: number }) => {
         transform: `scale(${scale})`,
         minHeight: "200px",
         zIndex,
-        marginTop: `${topOffset}px`,
+        marginTop: `${topOffset}%`,
       }}
     >
       <div className="p-4 flex flex-col gap-2">
