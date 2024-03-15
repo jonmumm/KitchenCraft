e67a35a1eace4273a280a3ca0df34c33d43ec438
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-03-15 14:02:00 -0700

    always show cards

--- src/app/@craft/components.client.tsx
+++ src/app/@craft/components.client.tsx
@@ -375,7 +375,7 @@ export const AddedTokens = () => {
 export const SuggestedRecipeCards = () => {
   const session$ = useSessionStore();
   const session = useStore(session$);
-  const numCards = Math.max(session.context.numCompletedRecipes, 6);
+  const numCards = Math.min(session.context.numCompletedRecipes, 6);
   const items = new Array(numCards).fill(0);
 
   return (
