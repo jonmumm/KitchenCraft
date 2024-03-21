2301081fb93221a14071aa8dd7a5d553aa03d128
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-03-21 15:16:39 -0700

    add settings to desktop view

--- src/app/@header/page.tsx
+++ src/app/@header/page.tsx
@@ -1,14 +1,40 @@
+import { Button } from "@/components/input/button";
+import {
+  SheetContent,
+  SheetOverlay,
+  SheetTrigger,
+} from "@/components/layout/sheet";
 import { TypeLogo } from "@/components/logo";
+import { MenuSheet } from "@/modules/main-menu/menu-sheet";
+import { GripVerticalIcon } from "lucide-react";
+import { MainMenu } from "../menu/components";
 import { CraftCTA } from "./components";
 
 export default async function Page({}) {
   return (
     <>
-      <div className="max-w-3xl mx-auto w-full h-[50vh] crafting:h-auto">
+      <div className="max-w-3xl mx-auto w-full h-[50vh] crafting:h-auto relative">
+        <div className="hidden lg:block absolute h-full right-4 top-8">
+          <MenuSheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <GripVerticalIcon />
+              </Button>
+            </SheetTrigger>
+            <SheetOverlay />
+            <SheetContent side="right" className="p-4">
+              <div className="flex flex-col gap-2 py-4">
+                <MainMenu />
+              </div>
+            </SheetContent>
+          </MenuSheet>
+        </div>
         <div className={`flex flex-col h-full justify-between p-4`}>
           <TypeLogo className="h-20 crafting:hidden" />
           <div className="flex flex-col gap-1 w-full crafting:max-w-3xl crafting:mx-auto">
-            <h2 className="text-2xl font-medium crafting:hidden">What&apos;s for dinner?</h2>
+            <h2 className="text-2xl font-medium crafting:hidden">
+              What&apos;s for dinner?
+            </h2>
             <p className="crafting:hidden text-muted-foreground text-sm mb-2">
               ⚡️ Instantly create personalized recipes.
             </p>
