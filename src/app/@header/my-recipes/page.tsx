6e747ec2a2370875260ef879da53f647eb3f2070
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-04-19 06:28:37 -0700

    fix crafting on my-recipes page

--- src/app/@header/my-recipes/page.tsx
+++ src/app/@header/my-recipes/page.tsx
@@ -1,9 +1,5 @@
-import { getProfileByUserId } from "@/db/queries";
-import { getCurrentUserId } from "@/lib/auth/session";
-import BasicHeader from "../components";
+import { HeaderWithInput } from "../components";
 
 export default async function Default() {
-  const userId = await getCurrentUserId();
-  const profile = userId ? await getProfileByUserId(userId) : undefined;
-  return <BasicHeader profile={profile} />;
+  return <HeaderWithInput />;
 }
