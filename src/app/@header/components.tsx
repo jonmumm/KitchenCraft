4c00ac50e043144aa50cd7a1243e352aa88977d7
Jonathan Mumm <jonmumm@users.noreply.github.com>
2024-04-20 15:19:59 -0700

    Update components.tsx

--- src/app/@header/components.tsx
+++ src/app/@header/components.tsx
@@ -91,7 +91,7 @@ export async function HeaderWithInput({
                 <ChefHatIcon className="transitioning:hidden" />
                 <Loader2Icon className="hidden transitioning:block animate-spin" />
                 <span>
-                  {profile?.profileSlug ? profile.profileSlug : "My Recipes"}
+                  {profile?.profileSlug ? profile.profileSlug : "My Cookbook"}
                 </span>
               </Badge>
             </div>
