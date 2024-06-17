547b72c198d101f0ba4c3499cfa57fbe5f36a30a
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-06-17 06:08:09 -0700

    logs

--- src/components/my-recipe-lists.tsx
+++ src/components/my-recipe-lists.tsx
@@ -16,15 +16,18 @@ import { Skeleton } from "./display/skeleton";
 
 export const MyRecipeLists = () => {
   const recentListIds = usePageSessionSelector(selectRecentListIds);
+  console.log({ recentListIds });
   const numEmpty = Math.max(3 - recentListIds.length, 0);
 
   return (
     <div className="relative h-24">
       <div className="absolute top-0 w-screen left-1/2 transform -translate-x-1/2 z-10 flex flex-row justify-center">
         <div className="carousel carousel-center pl-2 pr-2 space-x-2">
-          {recentListIds.filter((_, idx) => idx < 3).map((listId) => {
-            return <MyRecipeListCardById id={listId} key={listId} />;
-          })}
+          {recentListIds
+            .filter((_, idx) => idx < 3)
+            .map((listId) => {
+              return <MyRecipeListCardById id={listId} key={listId} />;
+            })}
           <MyRecipeListCardBySlug slug="make-later" />
           <MyRecipeListCardBySlug slug="liked" />
           <MyRecipeListCardBySlug slug="recently-shared" />
