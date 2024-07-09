402b06698b1d661c228eaf913af64b560c77d1cf
Jonathan Mumm <jonathanrmumm@gmail.com>
2024-07-09 05:52:48 -0700

    fix slug collisions

--- src/lib/slug.ts
+++ src/lib/slug.ts
@@ -2,7 +2,7 @@ import { sentenceToSlug } from "./utils";
 
 export const getSlug = ({ id, name }: { id: string; name: string }) => {
   const str = sentenceToSlug(name);
-  const idSlug = id.toLowerCase().slice(0, 5);
+  const idSlug = id.replace("-", "");
   const slug = `${idSlug}-${str}`;
 
   return slug;
