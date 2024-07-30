18ab1a3e6314201d00b5d8e42fa57442b7f1461a
QuentinLaco <quentin.lacornerie@gmail.com>
2024-07-30 17:19:41 -0400

    remove intro text animated and replace by simple text (#36)

--- src/app/quiz/intro/page.tsx
+++ src/app/quiz/intro/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Button } from "@/components/input/button";
-import AnimatedText from "@/components/typography/animated-text";
-import Delay from "@/components/util/delay";
 import { ChevronsRightIcon, Loader2Icon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
@@ -22,53 +20,26 @@ const Intro: React.FC = () => {
 
   return (
     <div className="max-w-md mx-auto w-full px-4 flex flex-col gap-4">
-      <div className="flex flex-col justify-center h-full md:mx-auto rounded-xl bg-gradient-to-r from-blue-500 to-blue-700 w-full p-4">
-        <h2 className="text-2xl font-semibold mb-6 leading-7">
-          <AnimatedText
-            text="Craft your ultimate recipe library."
-            baseSpeed={60}
-            punctDelay={600}
-            delay={1050}
-          />
-        </h2>
-        <div className="text-lg mb-4 font-semibold leading-7">
-          <AnimatedText
-            text="Welcome to KitchenCraft AI. This brief quiz will help KitchenCraft understand your goals, preferences and interests."
-            baseSpeed={60}
-            punctDelay={600}
-            delay={2250}
-          />
-        </div>
-        <div className="text-lg font-semibold">
-          <AnimatedText
-            text="Ready to begin?"
-            baseSpeed={35}
-            punctDelay={400}
-            delay={6250}
-          />
-        </div>
-      </div>
-      <Delay delay={7000}>
-        <div className="mt-2 max-w-md w-full p-4 mx-auto text-lg font-semibold sticky bottom-4">
-          <Button
-            className="text-lg font-semibold w-full"
-            size="xl"
-            onClick={handleStart}
-            disabled={loading}
-          >
-            {loading ? (
-              <>
-                Loading <Loader2Icon className="ml-2 animate-spin" size={16} />
-              </>
+      <h2 className="text-2xl semi-bold mt-16">Discover your ideal recipes, personalised to your unique tastes and preferences.</h2>
+      <div className="mt-16 max-w-md w-full p-4 mx-auto text-lg font-semibold sticky bottom-4">
+        <Button
+          className="text-lg font-semibold w-full"
+          size="xl"
+          onClick={handleStart}
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              Loading <Loader2Icon className="ml-2 animate-spin" size={16} />
+            </>
             ) : (
-              <>
-                Start Quiz
-                <ChevronsRightIcon className="ml-2" />
-              </>
-            )}
-          </Button>
-        </div>
-      </Delay>
+            <>
+              Start Quiz
+              <ChevronsRightIcon className="ml-2" />
+            </>
+          )}
+        </Button>
+      </div>
     </div>
   );
 };
